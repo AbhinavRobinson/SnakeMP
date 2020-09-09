492681fd5691d0f8fbcaae6fa12a0ad7d505ea9c
Abhinav Robinson <32090909+AbhinavRobinson@users.noreply.github.com>
2020-09-09 23:54:06 +0530

    🚚(feat)#2: Canvas Ready
    Major: Canvas Initialized and Drawn
    Minor: Keystrokes recognized, JS Commented for future ref.

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const gameScreen = document.getElementById('gameScreen');
+/*
+ * @design Color Styles
+ */
+const BG_COLOR = '#231f20';
+const SNAKE_COLOR = "#c2c2c2";
+const FOOD_COLOR = "#e66916";
 
+/*
+ * @variable Get gameScreen Element from HTML
+ */
+ const gameScreen = document.getElementById('gameScreen');
+
+
+/* 
+ * @function Initialization: creates canvas 600x600
+ */
 let canvas,ctx;
-
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
     canvas.width = canvas.height = 600;
+    
+    // Fills Canvas BG with Black-ish color
+    ctx.fillStyle = BG_COLOR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Activates when key pressed down, calls keydown function
+    document.addEventListener('keydown', keydown);
 }
+/* @EOF : Init() */
+
+
+/* 
+ * @function Keydown Event: Returns keyCode of pressed key in log
+ */
+function keydown(e) {
+    console.log(e.keyCode);
+}
+/* EOF : keydown() */
+
+/*
+ * @call Initializing Canvas
+ */
+init()
